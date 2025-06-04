783007cb4cc201f0b1232259e66af1625737581b
Khalid Wildan Lazuardi <khalidlazuardi52@gmail.com>
2025-06-04 13:18:20 +0700

    ci dan integrasi jest

---
+++ jest.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"]
+};
---
+++ src/__tests__/simple.test.js
@@ -0,0 +1,3 @@
+test('Simple test works', () => {
+  expect(1 + 1).toBe(2);
+});

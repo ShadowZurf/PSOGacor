5b74723f553bf39ec64e98b7754a0477f7c100f6
Khalid Wildan Lazuardi <khalidlazuardi52@gmail.com>
2025-06-05 15:14:30 +0700

    tes pipeline

--- src/__tests__/dashboard.test.js
+++ src/__tests__/dashboard.test.js
@@ -6,7 +6,7 @@ describe("Dashboard Page Mahasiswa", () => {
   it("renders dashboard page", () => {
     render(<DashboardPage />);
     // Sesuaikan dengan text yang memang ada di UI
-    expect(screen.getByText(/Selamat datang di ITS-OK/i)).toBeInTheDocument();
+    expect(screen.getByText(/Selamat datang/i)).toBeInTheDocument();
     expect(screen.getByText(/Pilih Layanan Sesuai Kebutuhan Anda/i)).toBeInTheDocument();
     expect(screen.getByText(/Daftar Konsultasi Offline/i)).toBeInTheDocument();
     expect(screen.getByRole('heading', { name: /Lagu Tenang/i, level: 3 })).toBeInTheDocument();
